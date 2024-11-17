e7035f7431e5dda9e839bb6d186db65448ae9dff
AbdulazizRus <coderdim@gmail.com>
2024-11-17 12:57:36 +0500

    cas

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import vercel from 'vite-plugin-vercel';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vercel()],
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Build natijasi uchun papka
+  },
+  server: {
+    port: 3000, // Lokal server porti
+  },
 });
