db6cb39f0315b81fe026dd8a1dfc65c94b9bc352
AbdulazizRus <coderdim@gmail.com>
2024-11-17 12:51:50 +0500

    fix config problem

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,11 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import vercel from 'vite-plugin-vercel';
 
+// https://vite.dev/config/
 export default defineConfig({
   server: {
     port: import.meta.env.VITE_PUBLIC_PORT,
   },
-  plugins: [vercel()],
+  plugins: [react(), vercel()],
 });
