e00549f33b2938facb77d36d15dd193191dc0008
AbdulazizRus <coderdim@gmail.com>
2024-11-27 22:53:24 +0500

    asasas

--- server/models/postback.model.js
+++ server/models/postback.model.js
@@ -1,15 +1,18 @@
-const postbackSchema = new mongoose.Schema({
-  name: { type: String, default: '' },
-  url: { type: String, required: true },
-  method: { type: String, enum: ['GET', 'POST'], required: true },
-  events: {
-    registration: { type: Boolean, default: false },
-    firstPayment: { type: Boolean, default: false },
-    subscription: { type: Boolean, default: false },
-    unsubscribe: { type: Boolean, default: false },
-    payment: { type: Boolean, default: false },
+const postbackSchema = new mongoose.Schema(
+  {
+    name: { type: String, default: '' },
+    url: { type: String, required: true },
+    method: { type: String, enum: ['GET', 'POST'], required: true },
+    events: {
+      registration: { type: Boolean, default: false },
+      firstPayment: { type: Boolean, default: false },
+      subscription: { type: Boolean, default: false },
+      unsubscribe: { type: Boolean, default: false },
+      payment: { type: Boolean, default: false },
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Postback = mongoose.model('Postback', postbackSchema);
 
